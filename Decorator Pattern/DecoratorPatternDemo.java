40ef9d5bcf131206c904462cc19b2c99457841fb
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-09 15:40:28 +0530

    Create DecoratorPatternDemo.java

---
+++ Decorator Pattern/DecoratorPatternDemo.java
@@ -0,0 +1,12 @@
+public class DecoratorPatternDemo {
+    public static void main(String[] args) {
+        Coffee coffee = new SimpleCoffee();
+        System.out.println(coffee.getDescription() + " $" + coffee.cost());
+
+        Coffee milkCoffee = new MilkDecorator(new SimpleCoffee());
+        System.out.println(milkCoffee.getDescription() + " $" + milkCoffee.cost());
+
+        Coffee sugarMilkCoffee = new SugarDecorator(new MilkDecorator(new SimpleCoffee()));
+        System.out.println(sugarMilkCoffee.getDescription() + " $" + sugarMilkCoffee.cost());
+    }
+}
