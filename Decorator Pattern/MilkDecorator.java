73075a9c1d71cb4167f8accf34f4373967030399
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-09 15:38:28 +0530

    Create MilkDecorator.java

---
+++ Decorator Pattern/MilkDecorator.java
@@ -0,0 +1,17 @@
+//Concrete decorater
+
+public class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee decoratedCoffee) {
+        super(decoratedCoffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double cost() {
+        return decoratedCoffee.cost() + 1.0;
+    }
+}
