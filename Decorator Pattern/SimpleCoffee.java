37097fc84dd154aeecbfa2d016fb5c0a8b61fa19
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-09 15:36:22 +0530

    Create SimpleCoffee.java

---
+++ Decorator Pattern/SimpleCoffee.java
@@ -0,0 +1,13 @@
+// Concrete Component (SimpleCoffee)
+
+public class SimpleCoffee implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 5.0;
+    }
+}
