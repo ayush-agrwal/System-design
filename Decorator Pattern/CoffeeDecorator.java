52eac8905c216a07fd56e184cf705fef99fc1253
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-09 15:37:22 +0530

    Create CoffeeDecorator.java

---
+++ Decorator Pattern/CoffeeDecorator.java
@@ -0,0 +1,18 @@
+// Decorator (CoffeeDecorator)
+public abstract class CoffeeDecorator implements Coffee {
+    protected Coffee decoratedCoffee;
+
+    public CoffeeDecorator(Coffee decoratedCoffee) {
+        this.decoratedCoffee = decoratedCoffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+
+    @Override
+    public double cost() {
+        return decoratedCoffee.cost();
+    }
+}
