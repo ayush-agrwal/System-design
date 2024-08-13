56beb0444ac01be36bff1d53258e4fcfe602ab7d
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2024-08-14 00:25:00 +0530

    Create LogProcessor.java

---
+++ Chain of Responsibility/LLD_chain_of_Responsibility/src/com/chainofResponse/LogProcessor.java
@@ -0,0 +1,23 @@
+package com.chainofResponse;
+
+public abstract class LogProcessor {
+
+	public static int INFO=1;
+	public static int DEBUG=2;
+	public static int ERROR=3;
+	
+	LogProcessor nextLogProcessor;
+	
+	public LogProcessor(LogProcessor logProcessor) {
+		this.nextLogProcessor=logProcessor;
+	}
+	
+	public void log(int loglevel, String message) {
+		
+		if(nextLogProcessor !=null) {
+			nextLogProcessor.log(loglevel, message);
+		}
+	}
+	
+	
+}
